3cd8a7ef31b4ea154706dc6c1cb2dee5037706e1
Gamma <GammaFunction@vivaldi.net>
2023-07-27 19:40:42 -0500

    Fix --dry-run description

--- contrib/pass2bw.zsh
+++ contrib/pass2bw.zsh
@@ -10,7 +10,7 @@ typeset -g help_text="Usage: $0 [OPTIONS] pass-name ...
 OPTIONS:
 	-A, --all               Import all passwords, ignore the commnd line pass-names
 	    --session SESSION   Set BW_SESSION (see \`bw unlock --help\`)
-	-n, --dry-run           Set BW_SESSION (see \`bw unlock --help\`)
+	-n, --dry-run           Write out the json that would be passed to \`bw encode\`
 	-v, --verbose           Increase verbosity
 	-h, --help              Show this help
 "
